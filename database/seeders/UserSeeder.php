4599bdc17720b85e5f6e3a3180a256f8547b9ef4
Ackyras <45270846+Ackyras@users.noreply.github.com>
2023-03-29 04:05:08 +0700

    chore : init auth (#2)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $users = [
+            [
+                'name'      =>  'admin',
+                'email'     =>  'admin@admin',
+                'password'  =>  bcrypt('password')
+            ],
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
